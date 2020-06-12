da474ade0230e5edaa8569382e40b09e2736816c
taoyouyou <853094838@qq.com>
2020-06-12 16:00:29 +0800

    优化代码

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import App from './App'
-// import './assets/style/main.css'
-// import 'font-awesome/css/font-awesome.min.css'
 
 import vueKeyboard from './lib/index.js'
 
@@ -11,7 +9,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
   components: { App },
   template: '<App/>'
 })
